2919e99b569bdbb38e4252fcb4fb256703f49610
Steven Degutis <sbdegutis@gmail.com>
2020-06-19 17:06:05 -0500

    Second

---
+++ index.js
@@ -0,0 +1,36 @@
+const path = require('path');
+const amd = require('./node_modules/monaco-editor/min/vs/loader.js');
+
+const editorEl = document.getElementById('editor');
+let editor;
+
+function uriFromPath(_path) {
+  var pathName = path.resolve(_path).replace(/\\/g, '/');
+  if (pathName.length > 0 && pathName.charAt(0) !== '/') {
+    pathName = '/' + pathName;
+  }
+  return encodeURI('file://' + pathName);
+}
+
+amd.require.config({
+  baseUrl: uriFromPath(path.join(__dirname, './node_modules/monaco-editor/min'))
+});
+
+// workaround monaco-css not understanding the environment
+self.module = undefined;
+
+amd.require(['vs/editor/editor.main'], () => {
+  editor = monaco.editor.create(editorEl, {
+    value: SAMPLE.trim(),
+    language: 'html',
+  });
+
+  // editorEl.style.height = (window.innerHeight) + 'px';
+  // editorEl.style.width = (window.innerWidth / 2) + 'px';
+  editor.layout();
+});
+
+const SAMPLE = `
+<b style="width: 90%">hello</b>
+<style>body #foo.bar { color: red; }</style>
+`;
--- main.js
+++ main.js
@@ -1,14 +1,15 @@
 const electron = require('electron');
-const path = require('path');
 
 electron.app.whenReady().then(() => {
   const win = new electron.BrowserWindow({
     width: 800,
     height: 600,
     webPreferences: {
-      preload: path.join(__dirname, 'preload.js'),
+      nodeIntegration: true,
     },
   });
+
+  win.webContents.toggleDevTools();
 
   win.loadFile('index.html');
 });
